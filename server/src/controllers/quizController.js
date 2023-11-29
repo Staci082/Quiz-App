37ad54b254394631c720255a3f9c579366fa88f1
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-11-30 00:49:17 +0100

    server added

--- server/src/controllers/quizController.js
+++ server/src/controllers/quizController.js
@@ -4,7 +4,6 @@ export async function getQuestions(req, res) {
     const { category } = req.params ;
     try {
         const questions = await Quiz.find({ category: `${category}` });
-        console.log('Retrieved Questions:', questions);
         res.json(questions);
     } catch (error) {
         console.log(error);
